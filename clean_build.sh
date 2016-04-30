6083ed7784a161be7312bb638b15a02c8e965633
Bryan Price <bprice@3-gis.com>
2016-04-29 20:46:52 -0500

    Project updates

--- clean_build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-(rm -Rf output/*) && make html && (sleep 2; open http://localhost:8000) & make serve
---
+++ cleanup.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+
+def remove(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+    else:
+        os.remove(path)
+
+directory = os.path.abspath(os.path.dirname(__file__))
+output_directory = os.path.join(directory, "output")
+files = [os.path.join(output_directory, x) for x in os.listdir(output_directory) if not x.startswith(".git")]
+map(remove, files)
